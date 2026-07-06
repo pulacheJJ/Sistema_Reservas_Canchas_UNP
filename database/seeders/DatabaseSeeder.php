0630d82fc494197c806fb8dd417c809c5cfa6bab
CarlosDev20 <ca179834@gmail.com>
2026-07-06 12:37:01 -0500

    featSe suben las modificaciones de ésta

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,15 +12,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\User::create([
-            'name' => 'Administrador UNP',
-            'codigo' => 'admin',
-            'role' => 'admin',
-            'password' => bcrypt('admin123'),
-        ]);
-
-        // Se elimina el estudiante de prueba para producción
         $this->call([
+            UserSeeder::class,
             CanchaSeeder::class,
         ]);
     }
