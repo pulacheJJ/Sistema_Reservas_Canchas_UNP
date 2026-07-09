069a352f74e43fb1a2e1b007ee8640f481fbd56f
Jaime Jonathan Pulache Pulache <jonapul08@gmail.com>
2026-07-09 03:20:41 -0500

    Fix migration

--- database/migrations/2026_07_04_222641_add_is_evento_to_reservas_table.php
+++ database/migrations/2026_07_04_222641_add_is_evento_to_reservas_table.php
@@ -9,13 +9,18 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
-        Schema::table('reservas', function (Blueprint $table) {
+   public function up(): void
+{
+    Schema::table('reservas', function (Blueprint $table) {
+        // Verificamos si la columna no existe antes de crearla
+        if (!Schema::hasColumn('reservas', 'is_evento')) {
             $table->boolean('is_evento')->default(false)->after('estado');
+        }
+        if (!Schema::hasColumn('reservas', 'titulo_evento')) {
             $table->string('titulo_evento')->nullable()->after('is_evento');
-        });
-    }
+        }
+    });
+}
 
     /**
      * Reverse the migrations.
